07f8a20199642f9a5ca71bc332bb7426245fb2f5
callumjhays <callumjhays@gmail.com>
2016-03-28 13:39:53 +1000

    Cleaning up

--- RPi_Server_Python/config.py
+++
@@ -1,7 +0,0 @@
-#Name: config.py
-#Author: John Board
-#Date: 11/02/2016
-#Description
-
-LOGGING_NAME = "AirHockeyRobot"
-
--- RPi_Server_Python/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-a = np.random.rand(480, 640, 3)  # create array of random values
-b = a.tobytes()
-c = np.fromstring(b).reshape(480, 640, 3)
-print(a)
-#print(b)
-print("cat")
-print(c)
-
